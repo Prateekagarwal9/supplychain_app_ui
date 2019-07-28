cc2f8d1d56c78bc9168d6437d02022667e3d0d5c
Prateekagarwal9 <39221904+Prateekagarwal9@users.noreply.github.com>
2019-07-28 23:51:53 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,4 +1,4 @@
 import os
 databricksname="https://eastus.azuredatabricks.net/"
 accesstoken="dapi57165fafe83f953de44e61ad26da1fcf"
-os.system("python3 databricks_linux/test324.py {} {}".format(databricksname, accesstoken))
+os.system("python3 /home/site/wwwroot/azure_blob_app/databricks_linux/test324.py {} {}".format(databricksname, accesstoken))
