20bf5cf27686dfa44ab0f50ec40218019e9408da
Prateekagarwal9 <39221904+Prateekagarwal9@users.noreply.github.com>
2019-07-28 22:44:00 +0530

    Update views.py

--- azure_blob_app/views.py
+++ azure_blob_app/views.py
@@ -151,7 +151,7 @@ def execute_all_func(form_data):
         dic2['parameters']['ResourceGroupName']['value'] = resourcegroup
         dic2['parameters']['ResourceGroupLocation']['value'] = rglocation
 
-        os.system("python3 databricks_linux/test324.py {} {}".format(databricksname, accesstoken))
+        os.system("python3 databricks_linux/test324.py {} {}".format(workspaceurl, accesstoken))
 
         with open(os.path.join(BASE_DIR, "KeyVaultParameters.json"), "w") as f:
             f.write(json.dumps(dic2))
